e49ba594a1d6b8b63ac9735e93eebf56a01f568a
kasiveni <59281455+kasiveni@users.noreply.github.com>
2025-05-06 19:09:47 +0530

    Migrations

---
+++ Migrations/20250506050242_AddAppointmentDateAndTimeFix.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicAppointment.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAppointmentDateAndTimeFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AppointmentDateTime",
+                table: "Appointments",
+                newName: "AppointmentDate");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "AppointmentTime",
+                table: "Appointments",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppointmentTime",
+                table: "Appointments");
+
+            migrationBuilder.RenameColumn(
+                name: "AppointmentDate",
+                table: "Appointments",
+                newName: "AppointmentDateTime");
+        }
+    }
+}
