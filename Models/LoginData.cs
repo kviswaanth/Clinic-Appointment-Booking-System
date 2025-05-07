100ccc04198c2faa99e626652e364bd16ac7c7f6
kasiveni <59281455+kasiveni@users.noreply.github.com>
2025-05-07 14:23:49 +0530

    Update LoginData.cs
    add Key

--- Models/LoginData.cs
+++ Models/LoginData.cs
@@ -1,7 +1,11 @@
-﻿namespace ClinicAppointment.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicAppointment.Models
 {
     public class LoginData
     {
+        [Key]
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
     }
